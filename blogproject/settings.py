8eac5d8c27faa8cd7cccc7540793311fd2804282
sakar-n <nirsakar@gmail.com>
2023-06-05 15:30:29 +0545

    collectstatic server

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "ckeditor",
     "ckeditor_uploader",
     "gunicorn",
+    "whitenoise.runserver_nostatic"
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     
 ]
 
@@ -124,10 +126,11 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = 'static/'
-STATIC_ROOT = '\blogproject\static\assets'
+STATIC_ROOT = 'static/assets'
 STATICFILES_DIRS =(
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
